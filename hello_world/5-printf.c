8967ef1eaffa078b066df0b6badd19c3b7aa400c
LCobham <lucascobham@hotmail.com>
2022-09-27 11:38:32 -0700

    Created 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - print specific string with printf
+ *
+ * Return: 0 if no error is present
+ */
+
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
