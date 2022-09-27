42affbc8633671915dd7a906ee4a59ae5a5a292b
LCobham <lucascobham@hotmail.com>
2022-09-27 11:34:09 -0700

    Created 4-puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints a specific string
+ *
+ * Return: 0 if no error is present
+ */
+
+int main(void)
+{
+	const char *s = "\"Programming is like building a multilingual puzzle";
+
+	puts(s);
+	return (0);
+}
