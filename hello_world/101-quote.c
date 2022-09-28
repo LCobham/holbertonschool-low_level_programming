58f8d5cffbae7bf5bcab476224d3a9757f70fc6f
LCobham <lucascobham@hotmail.com>
2022-09-28 05:15:12 -0700

    Attempted fix in 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,11 +2,12 @@
 /**
  * main - Prints specific string to std err
  *
- * Return: 
+ * Return: 0 if no errors are present
  */
 
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "%s",
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
