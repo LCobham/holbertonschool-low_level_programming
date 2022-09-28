83331c2a5732ceec869056253bc94857041fe7a9
LCobham <lucascobham@hotmail.com>
2022-09-28 05:17:09 -0700

    Another try at 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,12 +2,12 @@
 /**
  * main - Prints specific string to std err
  *
- * Return: 0 if no errors are present
+ * Return: 1 always
  */
 
 int main(void)
 {
 	fprintf(stderr, "%s",
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
