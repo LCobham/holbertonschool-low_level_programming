25b48e57d6d3b4d359c667b5ce173529d9d3cd38
LCobham <lucascobham@hotmail.com>
2022-09-29 11:06:47 -0700

    Created 3-islower.c, updated main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - determines if char is lowercase
+ *@c: int representing the ascii character to be evalated
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (97 <= c && 122 >= c)
+		return (1);
+	return (0);
+}
