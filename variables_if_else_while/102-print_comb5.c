5ce95c0e07e6070330ea418232bdcc1c8e0c2a83
LCobham <lucascobham@hotmail.com>
2022-09-29 05:43:48 -0700

    Attempt at 102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: 0 if no errors
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j, m ,n;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			m = 48;
+			while (m < 58)
+			{
+				n = 48;
+				while (n < 58)
+				{
+					if (m > i || (m == i && n > j))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(32);
+						putchar(m);
+						putchar(n);
+						
+						if (i == 57 && j == 56 && m == 57 && n == 57)
+						{
+							n++;
+							continue;
+						}
+
+						putchar(44);
+						putchar(32);
+					}
+					
+					n++;
+				}
+				m++;
+
+			}
+			j++;
+		}
+
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
