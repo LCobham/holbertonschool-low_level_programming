7cd1bcf5624036b8e089b847bc2f2d9c340822c1
LCobham <lucascobham@hotmail.com>
2022-09-29 11:20:40 -0700

    Created 4-isalpha, updated main.h

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - determines if input corresponds to alphabetic character
+ *
+ * @c: number to be evaluated
+ *
+ * Return: 1 if c is alphabetic character and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+
+	return (0);
+}
