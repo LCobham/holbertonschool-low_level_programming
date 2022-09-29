d9e6c901c894973323c5c2a995fed9f694d6589a
LCobham <lucascobham@hotmail.com>
2022-09-29 14:52:22 -0700

    Created 6-abs.c, updated main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an int
+ * @i: int of which the abs value is calculated
+ *
+ * Return: abs value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	return (-i);
+}
