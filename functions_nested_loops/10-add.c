2d3302b4dc1ba96beeb6327b39e0b0142e849401
LCobham <lucascobham@hotmail.com>
2022-09-29 16:44:11 -0700

    Created 10-add.c, updated main.h

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers passed as args
+ * @a: int that will be added to int b
+ * @b: int that will be added to int a
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
