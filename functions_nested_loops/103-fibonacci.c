98238e54d3da97ef192b1b41c6bffca967a00090
LCobham <lucascobham@hotmail.com>
2022-10-02 14:16:44 -0700

    Fixed 103-fibonacci.c

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -8,20 +8,17 @@
 int main(void)
 {
 	long i, j, sum;
-	int count;
 
 	sum = 0;
-	count = 2;
 	i = 1;
 	j = 2;
 
 	while (j < 4000000)
 	{
-		if (count % 2 == 0)
+		if (j % 2 == 0)
 			sum += j;
 		j += i;
 		i = j - i;
-		count++;
 	}
 	printf("%ld\n", sum);
 	return (0);
