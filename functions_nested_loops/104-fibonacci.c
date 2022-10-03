24c5a172968cf189887ec96bf3d09c6299f2b7ef
LCobham <lucascobham@hotmail.com>
2022-10-03 11:09:20 -0700

    Another attempt at 104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,52 @@
 
 int main(void)
 {
-	unsigned long i, j;
+	unsigned long i, j, first_half_j, second_half_j, first_half_i, second_half_i, res1, res2;
 	int count;
 
 	count = 2;
 	i = 1;
 	j = 2;
 
-	printf("%lu, ", i);
-	while (count < 99)
-	{
-		printf("%lu", j);
-		if (count != 98)
-			printf(", ");
 
-		j += i;
-		i = j - i;
-		count++;
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	while (count < 98)
+	{
+		if (count > 90)
+		{
+			if (count == 91)
+			{
+				first_half_j = j / 10000000000;
+				second_half_j = j % 10000000000;
+
+				first_half_i = i / 10000000000;
+				second_half_i = i % 10000000000;
+			}
+
+			printf("%lu", first_half_j + first_half_i);
+			printf("%lu", second_half_j + second_half_i);
+			if (count != 97)
+				printf(", ");
+
+			first_half_j += first_half_i;
+			second_half_j += second_half_i;
+			first_half_i = first_half_j - first_half_i;
+			second_half_i = second_half_j - second_half_i;
+
+			count++;
+		}
+		else
+		{
+			printf("%lu", j + i);
+			if (count != 98)
+				printf(", ");
+		
+		
+			j += i;
+			i = j - i;
+			count++;
+		}
 	}
 	printf("\n");
 	return (0);
