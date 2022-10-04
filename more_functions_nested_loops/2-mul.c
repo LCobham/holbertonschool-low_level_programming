a5f5651634d4c0c745c372e5d238b1ff6167af87
LCobham <lucascobham@hotmail.com>
2022-10-04 10:27:39 -0700

    Created 2-mul.c, updated main.h

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - returns the multiplication of both arguments
+ * @a: int which whill be multiplied
+ * @b: int which will be multiplied
+ *
+ * Return: the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
