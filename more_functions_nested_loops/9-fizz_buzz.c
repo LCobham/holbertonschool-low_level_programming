f52a2436cc4f9dc4f3c3e0a3fb9857a6927e000b
LCobham <lucascobham@hotmail.com>
2022-10-04 14:29:14 -0700

    Created 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i, count;
+
+	for (i = 1, count = 0; i < 101; i++, count++)
+	{
+		if (i % 3 == 0 && i % 15 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i % 15 != 0)
+		{
+			printf("Buzz");
+			if (count == 100)
+				printf("\n");
+			else
+				printf(" ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+}
