b62cf300a3a52d677aaa4897fabe79c582b9a418
LCobham <lucascobham@hotmail.com>
2022-10-05 08:07:19 -0700

    Added compiled object code instead of src file. Fixed

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int is_prime(long n);
+/**
+ * main - finds and prints the largest prime factor of 612852475143
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	long i;
+	long target = 612852475143, last;
+
+	last = target;
+	while (target % 2 == 0)
+	{
+		target /= 2;
+		last = 2;
+	}
+	for (i = 3; i <= target / 2 + 1; i += 2)
+	while (target % i == 0)
+	{
+		target /= i;
+		last = i;
+	}
+	if (is_prime(last) == 1)
+		printf("%ld\n", last);
+	return (0);
+}
+
+int is_prime(long n)
+{
+	long i;
+
+	if (n % 2 == 0)
+		return (0);
+	for (i = 3; i < n; i += 2)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
