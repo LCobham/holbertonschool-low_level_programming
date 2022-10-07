3cfbeac4baebc8b48cadd611625085fac7f54b28
LCobham <lucascobham@hotmail.com>
2022-10-07 05:33:34 -0700

    Created 7-puts2.c, updated main.h

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a given string
+ * @str: address of string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, len, n;
+
+	for (len = 0; *(str + len) != '\0'; len++)
+		continue;
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (i = 0; n + i < len; i++)
+		_putchar(*(str + n + i));
+	_putchar('\n');
+}
