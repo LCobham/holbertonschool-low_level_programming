0cf8a10fa7b705040be8682d0e0dfee75e12c329
LCobham <lucascobham@hotmail.com>
2022-10-11 05:33:40 -0700

    Created 100-rot13, updated main.h

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a str using rot13
+ * @s: pointer to str
+ *
+ * Return: pointer to encoded str
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char before[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char after[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; before[j] != '\0'; j++)
+		{
+			if (s[i] == before[j])
+			{
+				s[i] = after[j];
+				i++;
+				j = 0;
+			}
+		}
+	}
+	return (s);
+}
