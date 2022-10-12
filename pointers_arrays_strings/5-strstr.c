fbe142e2b0d195e20b51ace08617567d53466b45
LCobham <lucascobham@hotmail.com>
2022-10-12 12:43:23 -0700

    Fixed coding style in 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,22 +12,22 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, m;
 
-        if (needle[0] == '\0')
-                return (haystack);
+	if (needle[0] == '\0')
+		return (haystack);
 
-        for (i = 0; needle[i] != '\0'; i++)
-                continue;
+	for (i = 0; needle[i] != '\0'; i++)
+		continue;
 
-        for (m = 0; haystack[m] != '\0'; m++)
-        {
-                if (haystack[m] == needle[0])
-                {
-                    for (j = 1; j < i; j++)
-                    {
-                        if (needle[j] == haystack[m + j])
-                            return (&haystack[m]);
-                    }
-                }
-        }
-        return (NULL);	
+	for (m = 0; haystack[m] != '\0'; m++)
+	{
+		if (haystack[m] == needle[0])
+		{
+			for (j = 1; j < i; j++)
+			{
+				if (needle[j] == haystack[m + j])
+					return (&haystack[m]);
+			}
+		}
+	}
+	return (NULL);
 }
