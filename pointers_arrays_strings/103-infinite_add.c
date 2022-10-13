3655d77fda38ec098a431f59586aea2a357d599f
LCobham <lucascobham@hotmail.com>
2022-10-12 20:02:01 -0700

    Saving progress on 103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-void rev_string(char *s);
 int max(int a, int b);
 
 /**
@@ -14,21 +13,23 @@ int max(int a, int b);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, s1, s2, x, carry, m;
+	int i, j, s1, s2, carry, x, m, k;
 
 	for (i = 0; n1[i] != '\0'; i++)
 		continue;
 	for (j = 0; n2[j] != '\0'; j++)
 		continue;
 
-	if (max(i, j) + 2 > size_r)
+	k = max(i, j) + 2;
+	if (size_r < k)
 	{
 		r = 0;
 		return (r);
 	}
-
-	for (i--, j--; i >= 0 && j >= 0;)
+	r[k] = '\0';
+	for  (m = 0, carry = 0, i--, j--; k - 1 - m >= 0;)
 	{
+		x = carry;
 		if (i >= 0)
 		{
 			s1 = n1[i];
@@ -39,31 +40,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			s2 = n2[j];
 			j--;
 		}
-		if (s1 + s2 + x > '9' + '0')
+		if (s1 + s2 + x - '0' > '9')
 		{
 			carry = 1;
-			r[m] = s1 + s2 + x - 10 - '0';
-			m++;
-
+			r[k - 1 - m] = s1 + s2 + x - 10 - '0';
 		}
 		else
 		{
 			carry = 0;
-			r[m] = s1 + s2 + x - '0';
-			m++;
+			r[k - 1 - m] = s1 + s2 + x - '0';
 		}
-		x = carry;
+		m++;
 		s1 = 0;
 		s2 = 0;
 	}
-
-	if (x == 1)
-	{
-		r[m] = 1;
-		m++;
-	}
-	r[m] = '\0';
-	rev_string(r);
 	return (r);
 }
 
@@ -79,28 +69,4 @@ int max(int a, int b)
 	if (a > b)
 		return (a);
 	return (b);
-}
-
-
-/**
- * rev_string - reverse a string
- * @s: address of first char in the string
- *
- * Return: void
- */
-
-void rev_string(char *s)
-{
-	int i, len;
-	char c;
-
-	for (len = 0; *(s + len) != '\0'; len++)
-		continue;
-	len--;
-	for (i = 0; i <= len / 2; i++)
-	{
-		c = *(s + i);
-		*(s + i) = *(s + len - i);
-		*(s + len - i) = c;
-	}
 }
