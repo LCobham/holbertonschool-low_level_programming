aeb788418cdbb9148b2209798fc63deec9797595
LCobham <lucascobham@hotmail.com>
2022-10-17 07:10:11 -0700

    Final fix in 101-wildcmp.c

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -16,11 +16,8 @@ int wildcmp(char *s1, char *s2)
 	if (s1[0] == s2[0])
 		return (wildcmp(s1 + 1, s2 + 1));
 
-	if (s2[0] == '*')
-	{
-		while (s2[1] == '*')
-			s2++;
-	}
+	if (s2[0] == '*' && s2[1] == '*')
+		return (wildcmp(s1 , s2 + 1));
 
 	if (s2[0] == '*' && s2[1] != '\0' && s1[0] == '\0')
 		return (0);
