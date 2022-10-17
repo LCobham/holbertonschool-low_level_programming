61ca6ab304dd31774581c48c6b20e8884588db7b
LCobham <lucascobham@hotmail.com>
2022-10-17 10:00:58 -0700

    Fixed coding style in main.h

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -13,7 +13,7 @@ int _abs(int i);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
-int add (int a, int b);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void natural(void);
