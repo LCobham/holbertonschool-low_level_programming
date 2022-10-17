f095a9f6850251cec6f04ad2d8f9f70997ac4489
LCobham <lucascobham@hotmail.com>
2022-10-17 11:36:02 -0700

    Fixed coding style in 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: 0 on success
  */
 
-int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
