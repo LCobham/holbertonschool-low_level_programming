87f7cc303ee45389b1e2438c66cd0dc7f04156d2
LCobham <lucascobham@hotmail.com>
2022-10-17 13:13:09 -0700

    Modified 100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -15,10 +15,18 @@ int main(int argc, char *argv[])
 	int coins[5] = {25, 10, 5, 2, 1};
 
 
-	if (argc != 2 || atoi(argv[1]) < 0)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		if (*argv[i] == '-')
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
 	remaining = atoi(argv[1]);
 	res = 0;
