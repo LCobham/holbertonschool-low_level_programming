fd3a494770027e50a1dc5ed12ea3cd01324e94e1
LCobham <lucascobham@hotmail.com>
2022-10-17 12:02:47 -0700

    Added corner case in 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,12 +11,24 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; *(argv[i] + j) != '\0'; j++)
+		{
+			if (*(argv[i] + j) <= 48 && *(argv[i] + j) > 58 && *(argv[i] + j) != '\0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
 
 	for (i = 1, sum = 0; i < argc; i++)
