9ea9c624a404e08749e5ccf9f0727bafef263db8
LCobham <lucascobham@hotmail.com>
2022-10-18 08:53:30 -0700

    Trying something on 101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -11,23 +11,17 @@
 
 int main(void)
 {
-	char password[11];
+	int a, b, c;
 	time_t t;
 
 	srand((unsigned) time(&t));
 
-	password[0] = rand() % 57 + 71;
-	password[1] = rand() % 77 + 32;
-	password[2] = 'V';
-	password[3] = rand() % 25 + 102;
-	password[4] = rand() % 19 + 32;
-	password[5] = rand() % 6 + 121;
-	password[6] = rand() % 71 + 56;
-	password[7] = rand() % 46 + 32;
-	password[8] = rand() % 50 + 32;
-	password[9] = '2';
-	password[10] = '\0';
-
-	printf("%s", password);
+	for (b = 0, c = 2772; c > 122; b++)
+	{
+		a = (rand() % 125) + 1;
+		printf("%c", a);
+		c -= a;
+	}
+	printf("%c", c);
 	return (0);
 }
