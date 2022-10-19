a5753d6570d61cb1a89da77b0cae0f4fd905bcce
LCobham <lucascobham@hotmail.com>
2022-10-19 07:51:40 -0700

    Trying something in 1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,11 +12,14 @@ char *_strdup(char *str)
 	int i, len;
 	char *p = NULL;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (len = 0; str[len] != '\0'; len++)
 		continue;
 
 	p = malloc(len + 1);
-	if (p == NULL || str == NULL)
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		p[i] = str[i];
