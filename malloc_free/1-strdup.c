277916e94db978a86fb01d28ab35a183c89b663a
LCobham <lucascobham@hotmail.com>
2022-10-19 07:42:26 -0700

    Created 1-strdup.c, updated main.h

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - copy a string in another piece of memory
+ * @str: string to be copied
+ *
+ * Return: pointer to new string
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *p = NULL;
+
+	for (len = 0; str[len] != '\0'; len++)
+		continue;
+
+	p = malloc(len + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		p[i] = str[i];
+
+	return (p);
+}
