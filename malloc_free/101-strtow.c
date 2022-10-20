b29c71cc2f56cb97ac13fc55628ca4267c4fe76c
LCobham <lucascobham@hotmail.com>
2022-10-20 07:34:10 -0700

    Created 101-strtow.c, updated main.h

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * strtow - splits a string into words
+ * @str: string
+ *
+ * Return: pointer to array of strings
+ */
+
+char **strtow(char *str)
+{
+	int i, j, m, wc;
+	char **p;
+
+	if (strlen(str) == 0)
+		return (NULL);
+	for (i = 0, wc = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] == ' ')
+			i++;
+		else if ((str[i] > 32 && str[i] < 127) && (i == 0 || str[i - 1] == ' '))
+			wc++;
+	}
+	p = malloc((wc + 1) * sizeof(char *));
+	if (p == NULL)
+		return (NULL);
+	p[wc] = NULL;
+	for (i = 0, m = 0; i < wc && str[m] != '\0'; m++)
+	{
+		if (str[m] == ' ' && str[m + 1] == ' ')
+			continue;
+		else if ((str[m] > 32 && str[m] < 127) && (m == 0 || str[m - 1] == ' '))
+		{
+			for (j = 0; str[m + j] != ' ' && str[m + j] != '\0'; j++)
+				continue;
+			p[i] = malloc(j + 1);
+			if (p[i] == NULL)
+			{
+				for (m = 0; m < i; m++)
+					free(p[m]);
+				free(p);
+				return (NULL);
+			}
+			strncpy(p[i], &str[m], j);
+			p[i][j] = '\0';
+			i++;
+		}
+	}
+	return (p);
+}
