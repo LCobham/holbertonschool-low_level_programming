b2d8a828c9aca097b07f71a8c02c697b88e20e8e
LCobham <lucascobham@hotmail.com>
2022-10-22 10:42:52 -0700

    Changed 101-mul.c entirely

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char *mult_str(char *str1, char *str2);
-char *infinite_add(char *n1, char *n2);
-char *sum_array_str(char **array);
+char *mult_str(char *s1, char *s2);
 
 /**
  * main - multiply two positive numbers and print
@@ -36,127 +34,57 @@ int main(int argc, char **argv)
 		}
 	}
 	result = mult_str(argv[1], argv[2]);
-	printf("%s\n", result);
+	if (result[0] == '0')
+		printf("%s\n", &result[1]);
+	else
+		printf("%s\n", result);
 	free(result);
 	return (0);
 }
 
 /**
  * mult_str - multiply two positive numbers passed as string
- * @str1: string 1 (only digits)
- * @str2: string 2 (only digits)
+ * @s1: string 1 (only digits)
+ * @s2: string 2 (only digits)
  *
  * Return: pointer to str with result. Must be freed after usage.
  */
 
-char *mult_str(char *str1, char *str2)
+char *mult_str(char *s1, char *s2)
 {
-	int i, j, result_len, N, carry;
-	char *p;
+	int i, j, L1, L2, nchars, k, c;
+	char *p, *q;
 
-	result_len = strlen(str1) + strlen(str2);
-	if ((str1[0] - '0') * (str2[0] - '0') < 9)
-		       result_len--;	
+	L1 = strlen(s1);
+	L2 = strlen(s2);
+	nchars = L1 + L2;
 
-	p = malloc(result_len + 1);
+	p = malloc(nchars + 1);
 	if (p == NULL)
 	{
-		printf("Malloc failed\n");
-		exit(98);	
+		printf("Error\n");
+		exit(98);
 	}
-	p[result_len] = '\0';
+	memset(p, '0', nchars);
+	p[nchars] = '\0';
 
-	for (i = strlen(str2) - 1, carry = 0; i >= 0; i--)
+	for (i = L1 - 1; i >= 0; i--)
 	{
-		for (j = strlen(str1) - 1; j >= 0; j--)
+		c = 0;
+		k = nchars - 1 - (L1 - 1 - i);
+
+		for (j = L2 - 1; j >= 0; j--)
 		{
-			N = (str2[i] - '0') * (str1[j] - '0') + carry;
-			printf("N is %d\n", N);
-			p[--result_len] = (N % 10) + '0';
-			carry = (N / 10) % 10;
+			c += (s1[i] - '0') * (s2[j] - '0') + (p[k] - '0');
+			p[k--] = (c % 10) + '0';
+			c /= 10;
+		}
+		while (c > 0)
+		{
+			c += p[k] - '0';
+			p[k--] = (c % 10) + '0';
+			c /= 10;
 		}
 	}
-
 	return (p);
-}
-
-
-/**
- * infinite_add - add two numbers
- * @n1: pointer to number 1
- * @n2: pointer to number 2
- *
- * Return: pointer to result. Must be freed after usage.
- */
-
-char *infinite_add(char *n1, char *n2)
-{
-	int i, j, s1, s2, carry, x, m, k;
-	char *r;
-
-	i = strlen(n1);
-	j = strlen(n2);
-
-	if (i < j)
-		k = j + 2;
-	else 
-		k = i + 2;	
-
-	r = malloc(k - 1);
-	r[k - 1] = '\0';
-	for  (m = 0, carry = 0, i--, j--; m < k - 1;)
-	{
-		x = carry;
-		if (i >= 0)
-		{
-			s1 = n1[i];
-			i--;
-		}
-		if (j >= 0)
-		{
-			s2 = n2[j];
-			j--;
-		}
-		if (s1 + s2 + x - '0' > '9')
-		{
-			carry = 1;
-			r[k - m - 2] = s1 + s2 + x - 10 - '0';
-		}
-		else
-		{
-			carry = 0;
-			r[k - m - 2] = s1 + s2 + x - '0';
-		}
-		m++;
-		s1 = '0';
-		s2 = '0';
-	}
-	if (r[0] == '0')
-		for (m = 0; m < k - 1; m++)
-			r[m] = r[m + 1];
-	return (r);
-}
-
-
-/**
- * sum_array_str - sum numbers stored as strings in an array ending in NULL
- * @array: pointer to array of str (pointer pointer). Ends in NULL
- *
- * Return: one single string with result of sum. Result must be freed after it's used.
- */
-
-char *sum_array_str(char **array)
-{
-	int i;
-	char *result, *tmp;
-	char start[] = "0";
-
-	for (i = 0, result = start; array[i] != NULL; i++)
-	{
-		tmp = result;
-		result = infinite_add(tmp, array[i]);
-		if (tmp != start)
-			free(tmp);
-	}
-	return (result);
 }
