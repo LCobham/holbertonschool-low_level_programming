5393e786ad7d2bc77bf45bbc3542ee8b525b8af0
LCobham <lucascobham@hotmail.com>
2022-10-27 08:30:02 -0700

    Created 1-init_dog.c, updated dog.h

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type 'struct dog'
+ * @d: pointer to a struct dog
+ * @name: string with dog name
+ * @age: age of dog
+ * @owner: stringwith owner name
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}
