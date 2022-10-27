c1306483321e8ec0398a377ca580d900e00f1093
LCobham <lucascobham@hotmail.com>
2022-10-27 09:27:15 -0700

    First iteration of 4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Return: pointer to new do
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+
+	return (p);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - records name of dog, name of owner and age of dog
  * @name: string with name of dog
@@ -16,4 +18,5 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
