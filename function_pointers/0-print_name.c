822806f6c60eec7fc3058a25da3e18e6eb49266c
LCobham <lucascobham@hotmail.com>
2022-10-28 06:19:06 -0700

    Created directory, readme and 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name through another function
+ * @name: name to print
+ * @f: pointer to function that prints the name
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+This is a directory dedicated to the use of function pointers
