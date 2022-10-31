5d9475a561bcab3ff14f00da77126e6064cd6fab
LCobham <lucascobham@hotmail.com>
2022-10-31 11:16:27 -0700

    Fixed bug in 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -15,39 +15,39 @@ void print_all(const char * const format, ...)
 	char ch, *st;
 	va_list ap;
 
-	va_start(ap, format);
-
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		switch (format[i])
+		va_start(ap, format);
+		while (format[i] != '\0')
 		{
-			case 'c':
-				ch = (char) va_arg(ap, int);
-				printf("%c", ch);
-				break;
-			case 'i':
-				j = va_arg(ap, int);
-				printf("%d", j);
-				break;
-			case 'f':
-				fl = (float) va_arg(ap, double);
-				printf("%f", fl);
-				break;
-			case 's':
-				st = va_arg(ap, char *);
-				if (st == NULL)
-					st = "(nil)";
-				printf("%s", st);
-				break;
-			default:
-				i++;
-				continue;
-		}
-		if (format[i + 1] != '\0')
+			switch (format[i])
+			{
+				case 'c':
+					ch = (char) va_arg(ap, int);
+					printf("%c", ch);
+					break;
+				case 'i':
+					j = va_arg(ap, int);
+					printf("%d", j);
+					break;
+				case 'f':
+					fl = (float) va_arg(ap, double);
+					printf("%f", fl);
+					break;
+				case 's':
+					st = va_arg(ap, char *);
+					while (st == NULL)
+						st = "(nil)";
+					printf("%s", st);
+					break;
+				default:
+					i++;
+					continue;
+			}
+		if (format[i++ + 1] != '\0')
 			printf(", ");
-		i++;
-	}
-
+		}
 	va_end(ap);
+	}
 	printf("\n");
 }
