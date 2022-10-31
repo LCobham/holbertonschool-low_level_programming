7610460d9b01eb71104219789840f47c613e64ab
LCobham <lucascobham@hotmail.com>
2022-10-31 08:08:36 -0700

    Modifying 1-print_numbers.c to make it more concise

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,12 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int i;
 	va_list ap;
 
-	if (n == 0)
-	{
-		printf("\n");
-		return;
-	}
-
 	va_start(ap, n);
 	for (i = 0; i < (int) n; i++)
 	{
