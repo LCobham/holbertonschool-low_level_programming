285675b7fcbe6e1df0f0ebe573a095f64377fab7
LCobham <lucascobham@hotmail.com>
2022-11-01 07:00:15 -0700

    Created 100-hello_world.asm

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,18 @@
+
+section .text
+	global main
+
+main:
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, message
+	mov edx, message_len
+	int 0x80
+
+	mov eax, 0x1
+	mov ebx, 0x0
+	int 0x80
+
+section .data
+	message db "Hello World", 0xA
+	message_len equ $-message
