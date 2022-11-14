3c1c2bd4a2e24af5c0419f751fb8127631e2833f
LCobham <lucascobham@hotmail.com>
2022-11-14 07:54:18 -0800

    Created 1-list_len.c, modified lists.h and 0-print_list.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,23 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_list - print all elements of a linked list
+ * list_len - calculate n of elements in a linked list
  * @h: head of linked list
  *
  * Return: number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t counter = 0;
 	const list_t *tmp = h;
 
 	for (; tmp != NULL; tmp = tmp->next, counter++)
-	{
-		if (tmp->str != NULL)
-			printf("[%u] %s\n", tmp->len, tmp->str);
-		else
-			printf("[0] (nil)\n");
-	}
+		continue;
+
 	return (counter);
 }
