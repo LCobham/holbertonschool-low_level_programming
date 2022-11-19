d886a24d0e31caaa71fae2d54ce888038114140b
LCobham <lucascobham@hotmail.com>
2022-11-19 15:33:47 -0800

    Created 2-get_bit.c, updated main.h

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: number of which we're going to return the ith bit
+ * @index: index at which we want the value of that bit
+ *
+ * Return: value of bit at index or -1 on failure
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
