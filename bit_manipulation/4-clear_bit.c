9cf41d0ecfac1a3d227c82daaf3909f686face16
LCobham <lucascobham@hotmail.com>
2022-11-19 15:45:14 -0800

    Created 4-clear_bit.c, updated main.h

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * clear_bit - set a bit to 1 at a given index
+ * @n: pointer to number
+ * @index: self explanatory
+ *
+ * Return: 1 on success -1 on failure
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	if (get_bit(*n, index) == 1)
+		*n -= (1 << index);
+
+	return (1);
+}
+
+
+
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: number of which we're going to return the ith bit
+ * @index: index at which we want the value of that bit
+ *
+ * Return: value of bit at index or -1 on failure
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	return ((n >> index) & 1);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -10,5 +10,6 @@ void print_binary(unsigned long int n);
 int find_most_significant_bit(unsigned long int N);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
