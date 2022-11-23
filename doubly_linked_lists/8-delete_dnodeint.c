4a15de1601886b39123717e9529578c1aff72368
LCobham <lucascobham@hotmail.com>
2022-11-23 13:42:30 -0800

    Saving progress on taask 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - delete node of linked list
+ * @head: pointer to head
+ * @index: index at which to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp = NULL, *to_del;
+
+	if (!(*head))
+		return (-1);
+	
+	for (tmp = *head; tmp->next && i < index - 1;
+			tmp = tmp->next, i++)
+		continue;
+
+	if (i == index - 1 && tmp->next != NULL)
+	{
+		to_del = tmp->next;
+		tmp->next = to_del->next;
+		to_del->next->prev = tmp;
+		free(to_del);
+		return (1);
+	}
+	return (-1);
+}
