e37671ee1b70ce1333da048002d00901faa3d0ec
LCobham <lucascobham@hotmail.com>
2022-11-24 17:41:07 -0800

    Modified 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,16 +2,28 @@
 #include <stdlib.h>
 
 /**
- * hash_table_create - create a hash table of given array size
- * @size: size of the array
+ * hash_table_create - create a hash table of a given size
+ * @size: size of table
  *
- * Return: pointer to hash table
+ * Return: address to struct hash_table_t, or NULL if malloc fails
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *result = NULL;
+	hash_node_t **my_array = NULL;
 
-	table = malloc(sizeof(hash_node_t *) * size);
-	return (table);
+	result = malloc(sizeof(hash_table_t));
+	if (!result)
+		return (NULL);
+
+	result->size = size;
+	my_array = malloc(sizeof(hash_node_t *) * size);
+	if (!my_array)
+	{
+		free(result);
+		return (NULL);
+	}
+	result->array = my_array;
+	return (result);
 }
