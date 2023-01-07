003f719e79c7aec56ef2c94f43983b968af3604d
LCobham <lucascobham@hotmail.com>
2023-01-07 07:11:17 -0800

    Changes all over in 100-jump.c

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -12,29 +12,29 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int gap = sqrt(size), i, j;
+	int gap = sqrt(size), i, j, k;
 
 	if (!array)
 		return (-1);
 
-	for (i = 0; i < (int) size; i += gap)
+	for (i = 0, j = gap; i < (int) size; i += gap, j += gap)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		if (array[i] < value)
-			continue;
-		else
+		if (array[i] > value)
+			return (-1);
+		if (j >= (int) size || array[j] >= value)
 		{
-			printf("Value found between indexes [%d] and [%d]\n", i - gap, i);
-			for (j = i - gap; j >= 0 && j < i; j++)
+			printf("Value found between indexes [%d] and [%d]\n", i, j);
+			for (k = i; k < (int) size && k <= j; k++)
 			{
-				printf("Value checked array[%d] = [%d]\n", j, array[j]);
-				if (array[j] == value)
-					return (j);
+				printf("Value checked array[%d] = [%d]\n", k, array[k]);
+				if (array[k] == value)
+					return (k);
 			}
 			return (-1);
 		}
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
